8cb3147bf34c55452e8e09ac3ddce8bf485abc9c
gamemake-eng <mohinathsivaneswaran@gmail.com>
2024-09-21 18:38:08 -0400

    added a bouncing ball

--- main.c
+++ main.c
@@ -1,16 +1,40 @@
 #include <hagl_hal.h>
 #include <hagl.h>
 
-hagl_backend_t *display = hagl_init();
-hagl_color_t color = hagl_color(display, 255, 0, 0);
+
 int main() {
+	hagl_backend_t *display = hagl_init();
+	hagl_color_t color = hagl_color(display, 255, 0, 0);
+
 	int x = 0;
+	int y = 0;
+
+	int speed = 1;
+
+	int dx = 1;
+	int dy = 1;
+
+
+
 /* Main loop. */
 	while (1) {
+		x += dx*speed;
+		y += dy*speed;
 
-		x++;
+		if(x < 5){
+			dx = 1;
+		}else if (x>128-5) {
+			dx = -1;
+		}
+
+		if(y < 5) {
+			dy = 1;
+		}else if (y > 160-5){
+			dy = -1;
+		}
+
     		hagl_clear(display);
-    		hagl_fill_ellipse(display, x, 5, 5, 5, color);
+    		hagl_fill_ellipse(display, x, y, 5, 5, color);
 
     		hagl_flush(display);
 	};
