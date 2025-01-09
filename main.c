da17a91ff5bbadc42ad394902ac68b6e4ee3d88d
gamemake-eng <mohinathsivaneswaran@gmail.com>
2025-01-08 22:20:03 -0500

    added lerp and also made sure that the pico is using its hardware fpu.

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <font6x9.h>
 #include <string.h>
 #include <wchar.h>
+#include <pico/float.h>
 
 int main() {
 	hagl_backend_t *display = hagl_init();
@@ -16,36 +17,46 @@ int main() {
 	float speed = 1;
 
 	float size = 5;
+	float finalsize = 1;
 
 	float dx = 1;
 	float dy = 1;
 
-	
+	float t = 0;
 
+	
+	wchar_t text[100];
 	while (1) {
-		hagl_put_text(display, (wchar_t*) "text", 7, 7, color, font6x9);
+		t += 0.1;
 
 		x += dx*speed;
 		y += dy*speed;
 
-		if(x < size){
+		if(x < finalsize){
 			dx = 1;
-			size += 0.5;
-		}else if (x>160-size) {
+			size += 1;
+		}else if (x>160-finalsize) {
 			dx = -1;
-			size += 0.5;
+			size += 1;
 		}
 
-		if(y < size) {
+		if(y < finalsize) {
 			dy = 1;
-			size += 0.5;
-		}else if (y > 128-size){
+			size += 1;
+		}else if (y > 128-finalsize){
 			dy = -1;
-			size += 0.5;
+			size += 1;
 		}
+
+		finalsize = finalsize + (size - finalsize) * 0.07;
+
+		if(size > 30){size=1;}
 
     	hagl_clear(display);
-    	hagl_fill_ellipse(display, x, y, size, size, color);
+    	hagl_fill_ellipse(display, x, y, finalsize, finalsize, color);
+
+		swprintf(text, sizeof(text), L"size: %f", finalsize);
+		hagl_put_text(display, text, 7, 7, color, font6x9);
 
     	hagl_flush(display);
 	};
